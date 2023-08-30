3a6bdab3c1c19fc0f191b6914bc93c5017ce95a3
emrebulak <emrbrnn@gmail.com>
2023-08-30 18:12:48 +0300

    Entities added

---
+++ src/main/java/com/emrebulak/auditing/AuditingAwareBaseEntity.java
@@ -0,0 +1,4 @@
+package com.emrebulak.auditing;
+
+public class AuditingAwareBaseEntity {
+}
---
+++ src/main/java/com/emrebulak/data/entity/TodoEntity.java
@@ -0,0 +1,37 @@
+package com.emrebulak.data.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@Entity
+@Table(name = "todos")
+public class TodoEntity implements Serializable {
+    public static final Long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id",unique = true, nullable = false, insertable = true,updatable = false)
+    private Long id;
+
+    @Column(name = "title",length = 500, columnDefinition = "varchar(500) default 'Değer girilmedi'")
+    private String title;
+
+    @Column(name = "is_done")
+    private boolean isDone;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date systemDate;
+}
